e0cbcb0676184f78326ed14333fa4d78f68fb123
iammohammedkhasim <35887969+iammohammedkhasim@users.noreply.github.com>
2020-03-13 10:11:33 +0530

    Added g_access_token
    Added ,g_access_token to Access-Control-Allow-Headers

--- Server/src/main/java/services/CORSFilter.java
+++ Server/src/main/java/services/CORSFilter.java
@@ -11,7 +11,7 @@ public class CORSFilter implements ContainerResponseFilter {
 
         response.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
         response.getHttpHeaders().add("Access-Control-Allow-Headers",
-                "content-type,authorization");
+                "content-type,authorization,g_access_token");
         response.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
         response.getHttpHeaders().add("Access-Control-Allow-Methods",
                 "GET, POST, PUT, DELETE, OPTIONS, HEAD");
