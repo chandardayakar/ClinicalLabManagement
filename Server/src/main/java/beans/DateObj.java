9aaf061634b1c4659e828fbd0bae1c1f7c37dcc2
cgadi <dayakar18@gmail.com>
2020-04-03 17:23:47 +0530

    Converting Date Obj strings to int

--- Server/src/main/java/beans/DateObj.java
+++ Server/src/main/java/beans/DateObj.java
@@ -2,33 +2,33 @@ package beans;
 
 public class DateObj {
 
-    private String day;
+    private int day;
 
-    private String month;
+    private int month;
 
-    private String year;
+    private int year;
 
-    public String getDay() {
+    public int getDay() {
         return day;
     }
 
-    public void setDay(String day) {
+    public void setDay(int day) {
         this.day = day;
     }
 
-    public String getMonth() {
+    public int getMonth() {
         return month;
     }
 
-    public void setMonth(String month) {
+    public void setMonth(int month) {
         this.month = month;
     }
 
-    public String getYear() {
+    public int getYear() {
         return year;
     }
 
-    public void setYear(String year) {
+    public void setYear(int year) {
         this.year = year;
     }
 }
